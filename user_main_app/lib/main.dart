a5a1b63616a89fd1df94283c65f1b93a60bcbfdd
Sseankzs <sseankong52@gmail.com>
2023-07-08 04:01:40 +0800

    Added working buttons on the ETA Page

--- user_main_app/lib/main.dart
+++ user_main_app/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:user/eta.dart';
 import 'package:user/map.dart';
@@ -38,12 +37,27 @@ class _LogInState extends State<MainPage> {
     return Scaffold(
       body: const [ETA(), MapPage(), Schedule()][currentpage],
       bottomNavigationBar: NavigationBar(
+        backgroundColor: const Color.fromRGBO(220, 215, 201, 1),
+        animationDuration: const Duration(milliseconds: 500),
         destinations: const [
           NavigationDestination(
-              icon: Icon(Icons.roundabout_left), label: 'ETA'),
-          NavigationDestination(icon: Icon(Icons.map), label: 'Maps'),
+              icon: Icon(
+                Icons.roundabout_left,
+                color: Colors.black,
+              ),
+              label: 'ETA'),
           NavigationDestination(
-              icon: Icon(Icons.table_chart), label: 'Schedule'),
+              icon: Icon(
+                Icons.map,
+                color: Colors.black,
+              ),
+              label: 'Maps'),
+          NavigationDestination(
+              icon: Icon(
+                Icons.table_chart,
+                color: Colors.black,
+              ),
+              label: 'Schedule'),
         ],
         onDestinationSelected: (int index) {
           setState(() {
