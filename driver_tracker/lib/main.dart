7f0cde323d136f7159194d3b017dd2d39b0e79e2
xinghao2003 <wongxh45@gmail.com>
2023-07-08 15:44:16 +0800

    Driver Tracker Update
    Update system logic. Calculation using Route API.

--- driver_tracker/lib/main.dart
+++ driver_tracker/lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 import 'package:firebase_database/firebase_database.dart';
 
@@ -28,14 +29,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late GoogleMapController mapController;
-  final LatLng _center = const LatLng(45.521563, -122.677433);
-
   FirebaseDatabase database = FirebaseDatabase.instance;
   DatabaseReference? ref;
 
+  // Variables for tracking
   int? currentTrackingRoute;
   int? currentTrackingBus = 1;
+  int? currentTrackingSchedule = 1;
+  List<dynamic>? scheduleData;
+  int? currentStop = 0;
+  Map? currentStopData = {
+    "name": null,
+    "lat": 0.0,
+    "long": 0.0,
+  };
+  int? nextStop = 1;
+  Map? nextStopData = {
+    "name": null,
+    "lat": 0.0,
+    "long": 0.0,
+  };
+  int? duration = 0;
+  int? distance = 0;
 
   static const LocationSettings locationSettings = LocationSettings(
     accuracy: LocationAccuracy.high,
@@ -44,38 +59,166 @@ class _MyAppState extends State<MyApp> {
 
   StreamSubscription<Position>? positionStream;
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  void stopTrack() {
+    setState(() {
+      currentTrackingRoute = null;
+      currentTrackingBus = 1;
+      currentTrackingSchedule = 1;
+      currentStop = 0;
+      currentStopData = {
+        "name": null,
+        "lat": 0.0,
+        "long": 0.0,
+      };
+      nextStop = 1;
+      nextStopData = {
+        "name": null,
+        "lat": 0.0,
+        "long": 0.0,
+      };
+      duration = 0;
+      distance = 0;
+      scheduleData = null;
+      if (positionStream != null) positionStream?.cancel();
+    });
+    if (positionStream != null) positionStream?.cancel();
+    ref?.child("bus$currentTrackingBus").update({
+      'operation': false,
+    });
+    return;
   }
 
   void track(int route) {
+    // Reset previous route
     if (route == currentTrackingRoute) {
-      setState(() {
-        currentTrackingRoute = null;
-      });
-      if (positionStream != null) positionStream?.cancel();
+      stopTrack();
       return;
     }
-    currentTrackingRoute = null;
-    if (positionStream != null) positionStream?.cancel();
+
+    stopTrack();
 
     setState(() {
       currentTrackingRoute = route;
+      ref = FirebaseDatabase.instance.ref('route$currentTrackingRoute');
     });
 
+    // Start tracking
     positionStream = Geolocator.getPositionStream(locationSettings: locationSettings).listen((Position? position) async {
-      ref = FirebaseDatabase.instance.ref("route$currentTrackingRoute/bus$currentTrackingBus");
-      await ref?.update({
-        'lat': position?.latitude,
-        'long': position?.longitude,
-        'speed': position?.speed,
-        'heading': position?.heading,
-        'accuracy': position?.accuracy,
-        'altitude': position?.altitude,
+      if (scheduleData == null) {
+        var retrieve = await ref!.child('schedule$currentTrackingSchedule').get();
+        setState(() {
+          scheduleData = retrieve.value as List<dynamic>;
+        });
+        await ref?.child("bus$currentTrackingBus").update({
+          'operation': true,
+        });
+      }
+      //debugPrint(scheduleData.toString());
+
+      // retrieve data from firebase
+      if (currentStopData?["name"] == null) {
+        currentStopData?["name"] = scheduleData![currentStop!.toInt()]['name'];
+        currentStopData?["lat"] = scheduleData![currentStop!.toInt()]['lat'];
+        currentStopData?["long"] = scheduleData![currentStop!.toInt()]['long'];
+      }
+
+      // next stop calculation
+      if (nextStopData?["name"] == null) {
+        nextStopData?["name"] = scheduleData![nextStop!.toInt()]['name'];
+        nextStopData?["lat"] = scheduleData![nextStop!.toInt()]['lat'];
+        nextStopData?["long"] = scheduleData![nextStop!.toInt()]['long'];
+      }
+
+      //debugPrint(currentStopData?.toString());
+      //debugPrint(nextStopData?.toString());
+
+      http.Response response = await http.post(
+        Uri.parse('https://routes.googleapis.com/directions/v2:computeRoutes'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'X-Goog-Api-Key': 'AIzaSyAPCuddVW7ch26c7zek493XKjRz4bnkKoc',
+          'X-Goog-FieldMask': 'routes.duration,routes.distanceMeters',
+        },
+        body: '''{
+          "origin":{
+            "location":{
+              "latLng":{
+                "latitude": ${position?.latitude},
+                "longitude": ${position?.longitude}
+              }
+            }
+          },
+          "destination":{
+            "location":{
+              "latLng":{
+                "latitude": ${nextStopData?["lat"]},
+                "longitude": ${nextStopData?["long"]}
+              }
+            }
+          },
+          "travelMode": "DRIVE"
+        }''',
+      );
+
+      //debugPrint("Route API: ${response.statusCode}");
+      //debugPrint("Route API: ${response.body}");
+
+      var responseBody = jsonDecode(response.body);
+
+      setState(() {
+        distance = responseBody['routes'][0]['distanceMeters'];
+        duration = int.parse(responseBody['routes'][0]['duration'].replaceAll(RegExp('[^0-9]'), ''));
       });
-      debugPrint(position == null
-          ? 'Unknown data on $currentTrackingRoute'
-          : '$currentTrackingRoute : ${position.latitude.toString()}, ${position.longitude.toString()}');
+
+      // check if bus has reached next stop
+      double p1 = position?.latitude as double;
+      double p2 = position?.longitude as double;
+      double p3 = nextStopData?["lat"] as double;
+      double p4 = nextStopData?["long"] as double;
+      double distanceInMeters = Geolocator.distanceBetween(p1, p2, p3, p4);
+      debugPrint("Geolocator: $distanceInMeters");
+      if (distance == null || duration == null) {
+        distance = 0;
+        duration = 0;
+      }
+
+      if (distanceInMeters < (50 + (position!.speed / 100 * 25)) ||
+          distance! < (50 + (position.speed / 100 * 25)) ||
+          duration! < (10 + (position.speed / 100 * 10))) {
+        currentStop = nextStop;
+        nextStop = nextStop! + 1;
+
+        if (nextStop != scheduleData?.length) {
+          debugPrint("Stop reached, moving to next stop");
+          currentStopData = Map.from(nextStopData!);
+
+          nextStopData?["name"] = scheduleData![nextStop!.toInt()]['name'];
+          nextStopData?["lat"] = scheduleData![nextStop!.toInt()]['lat'];
+          nextStopData?["long"] = scheduleData![nextStop!.toInt()]['long'];
+        } else {
+          debugPrint("Last stop reached");
+          await ref?.child("bus$currentTrackingBus").update({
+            'operation': false,
+          });
+          stopTrack();
+        }
+        return;
+      }
+
+      // Update to firebase
+      await ref?.child("bus$currentTrackingBus").update({
+        'lat': position.latitude,
+        'long': position.longitude,
+        'speed': position.speed,
+        'heading': position.heading,
+        'accuracy': position.accuracy,
+        'altitude': position.altitude,
+        'timeStamp': position.timestamp.toString(),
+        'currentStop': currentStop,
+        'nextStop': nextStop,
+        'distance': distance,
+        'duration': duration,
+      });
     });
   }
 
@@ -95,6 +238,26 @@ class _MyAppState extends State<MyApp> {
             const SizedBox(height: 10),
             Text(
               currentTrackingRoute == null ? "Track Status: Not Tracking" : "Track Status: Tracking on Route $currentTrackingRoute Bus $currentTrackingBus",
+              style: const TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Current Stop: ${currentStopData?["name"]}",
+              style: const TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Next Stop: ${nextStopData?["name"]}",
+              style: const TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Estimated Duration: $duration s",
+              style: const TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              "Estimated Distance: $distance m",
               style: const TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 10),
