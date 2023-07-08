d9ddbd8d04b6325097fffc4fdf548ed46e181c75
Sseankzs <sseankong52@gmail.com>
2023-07-08 12:24:58 +0800

    ETA page update

--- user_main_app/lib/eta.dart
+++ user_main_app/lib/eta.dart
@@ -8,64 +8,82 @@ class ETA extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
+    return Column(
       children: [
         Column(
           children: [
             Container(
-              //alignment: const Alignment(0.01, 0.2),
+              color: const Color.fromRGBO(220, 215, 201, 1),
               padding: const EdgeInsets.fromLTRB(17, 38, 19, 2),
-              child: const Text.rich(
-                TextSpan(
-                  children: [
-                    TextSpan(
-                      text: 'Wel',
-                      style: TextStyle(
-                        color: Color.fromRGBO(63, 78, 79, 1),
-                        fontSize: 32,
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w700,
+              child: Container(
+                alignment: Alignment.topLeft,
+                padding: const EdgeInsets.fromLTRB(17, 38, 19, 2),
+                child: const Text.rich(
+                  TextSpan(
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: 'Wel',
+                        style: TextStyle(
+                          color: Color.fromRGBO(63, 78, 79, 1),
+                          fontSize: 36,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
-                    ),
-                    TextSpan(
-                      text: 'come',
-                      style: TextStyle(
-                        color: Color.fromRGBO(162, 123, 92, 1),
-                        fontSize: 32,
-                        fontFamily: 'Poppins',
-                        fontWeight: FontWeight.w700,
+                      TextSpan(
+                        text: 'come',
+                        style: TextStyle(
+                          color: Color.fromRGBO(162, 123, 92, 1),
+                          fontSize: 36,
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w700,
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
-
-            const Text(
-              'This page displays updated status of each route',
-            ),
             Container(
-              padding: const EdgeInsets.fromLTRB(1, 20, 1, 1),
+              color: const Color.fromRGBO(220, 215, 201, 1),
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.fromLTRB(34, 2, 1, 5),
+              child: const Text(
+                'This page displays updated status of each route',
+              ),
+            ),
+
+            Container(
+              color: const Color.fromRGBO(220, 215, 201, 1),
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.fromLTRB(34, 20, 1, 1),
               child: const Text(
                 'ETA Status',
                 textAlign: TextAlign.left,
                 style: TextStyle(
                   color: Colors.black,
-                  fontSize: 10,
+                  fontSize: 16,
                   fontFamily: 'Poppins',
-                  fontWeight: FontWeight.w700,
+                  fontWeight: FontWeight.w800,
                 ),
               ),
             ),
 
-            const Text(
-              'Internal Shuttle Bus',
-              textAlign: TextAlign.left,
-              style: TextStyle(fontSize: 10, fontFamily: 'Poppins'),
+            Container(
+              color: const Color.fromRGBO(220, 215, 201, 1),
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.fromLTRB(34, 20, 1, 1),
+              child: const Text(
+                'Internal Shuttle Bus',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 12, fontFamily: 'Poppins'),
+              ),
             ),
+
             //First Container
             Positioned(
               child: Container(
+                color: const Color.fromRGBO(220, 215, 201, 1),
                 alignment: Alignment.center,
                 padding: const EdgeInsets.fromLTRB(20, 5, 20, 20),
                 child: Column(
@@ -84,35 +102,59 @@ class ETA extends StatelessWidget {
                       },
                       child: Card(
                         child: Container(
-                          width: 315,
-                          height: 50,
-                          padding: const EdgeInsets.all(20),
-                          decoration: ShapeDecoration(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
+                            width: 350,
+                            height: 50,
+                            padding: const EdgeInsets.fromLTRB(15, 1, 15, 1),
+                            decoration: ShapeDecoration(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              shadows: const [
+                                BoxShadow(
+                                    color: Colors.grey,
+                                    blurRadius: 4,
+                                    offset: Offset(0, 4)),
+                              ],
+                              color: Colors.white,
                             ),
-                            shadows: const [
-                              BoxShadow(
-                                  color: Colors.black,
-                                  blurRadius: 4,
-                                  offset: Offset(0, 4)),
-                            ],
-                            color: Colors.white,
-                          ),
-                        ),
+                            child: const Row(
+                              children: [
+                                Icon(
+                                  Icons.circle_rounded,
+                                  size: 12,
+                                  color: Colors.red,
+                                ),
+                                Text(
+                                  'Next Stop: ',
+                                  textAlign: TextAlign.left,
+                                ),
+                                SizedBox(width: 140),
+                                Text(
+                                  'ETA - 00:00PM',
+                                  textAlign: TextAlign.left,
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            )),
                       ),
                     ),
                   ],
                 ),
               ),
             ),
-            const Text(
-              'External Shuttle Bus (Seri Iskandar)',
-              textAlign: TextAlign.left,
-              style: TextStyle(fontSize: 10, fontFamily: 'Poppins'),
+            Container(
+              color: const Color.fromRGBO(220, 215, 201, 1),
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.fromLTRB(34, 20, 1, 1),
+              child: const Text(
+                'External Shuttle Bus (Seri Iskandar)',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 12, fontFamily: 'Poppins'),
+              ),
             ),
             //Second Container
             Container(
+              color: const Color.fromRGBO(220, 215, 201, 1),
               alignment: Alignment.center,
               padding: const EdgeInsets.fromLTRB(20, 5, 20, 30),
               child: Column(
@@ -131,7 +173,7 @@ class ETA extends StatelessWidget {
                     },
                     child: Card(
                       child: Container(
-                        width: 315,
+                        width: 350,
                         height: 50,
                         padding: const EdgeInsets.all(20),
                         decoration: ShapeDecoration(
@@ -140,7 +182,7 @@ class ETA extends StatelessWidget {
                           ),
                           shadows: const [
                             BoxShadow(
-                                color: Colors.black,
+                                color: Colors.grey,
                                 blurRadius: 4,
                                 offset: Offset(0, 4)),
                           ],
@@ -152,13 +194,19 @@ class ETA extends StatelessWidget {
                 ],
               ),
             ),
-            const Text(
-              'External Shuttle Bus (Stn 18)',
-              textAlign: TextAlign.left,
-              style: TextStyle(fontSize: 10, fontFamily: 'Poppins'),
+            Container(
+              color: const Color.fromRGBO(220, 215, 201, 1),
+              alignment: Alignment.topLeft,
+              padding: const EdgeInsets.fromLTRB(34, 20, 1, 1),
+              child: const Text(
+                'External Shuttle Bus (Stn 18)',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontSize: 12, fontFamily: 'Poppins'),
+              ),
             ),
             //Third Container
             Container(
+              color: const Color.fromRGBO(220, 215, 201, 1),
               alignment: Alignment.center,
               padding: const EdgeInsets.fromLTRB(20, 5, 20, 30),
               child: Column(
@@ -177,7 +225,7 @@ class ETA extends StatelessWidget {
                     },
                     child: Card(
                       child: Container(
-                        width: 315,
+                        width: 350,
                         height: 50,
                         padding: const EdgeInsets.all(20),
                         decoration: ShapeDecoration(
@@ -186,7 +234,7 @@ class ETA extends StatelessWidget {
                           ),
                           shadows: const [
                             BoxShadow(
-                                color: Colors.black,
+                                color: Colors.grey,
                                 blurRadius: 4,
                                 offset: Offset(0, 4)),
                           ],
@@ -198,6 +246,18 @@ class ETA extends StatelessWidget {
                 ],
               ),
             ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              height: 204,
+              color: const Color.fromRGBO(220, 215, 201, 1),
+              child: const Text(
+                'ETA - Estimated Arrival Time',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Poppins',
+                    fontSize: 13),
+              ),
+            )
           ],
         ),
       ],
